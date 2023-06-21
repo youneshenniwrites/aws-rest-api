958f81d5b18c989926b30eeca02da7afc786dc79
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-21 16:15:37 +0100

    specify runtime for lambda

--- lib/aws-rest-api-stack.ts
+++ lib/aws-rest-api-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from "aws-cdk-lib";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction as AWSLambdaFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { join } from "path";
 
@@ -7,10 +8,11 @@ export class AwsRestApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     const createBLogPostLambdaName = "createBLogPostHandler";
-    const createBLogPostLambda = new NodejsFunction(
+    const createBLogPostLambda = new AWSLambdaFunction(
       this,
       createBLogPostLambdaName,
       {
+        runtime: Runtime.NODEJS_18_X,
         entry: join(__dirname, "lambdas", "blog-post-handler.ts"),
         handler: createBLogPostLambdaName,
         functionName: createBLogPostLambdaName,
