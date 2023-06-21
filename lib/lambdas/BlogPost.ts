21370eabc551f2cd72551c16d9239a761bd5d2b8
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-21 18:13:32 +0100

    define create blog post lambda function

---
+++ lib/lambdas/BlogPost.ts
@@ -0,0 +1,7 @@
+export interface BlogPost {
+  id: string;
+  title: string;
+  author: string;
+  content: string;
+  createdAt: string;
+}
--- lib/lambdas/blog-post-handler.ts
+++ lib/lambdas/blog-post-handler.ts
@@ -1,6 +1,12 @@
-export async function createBLogPostHandler() {
+import { APIGatewayEvent } from "aws-lambda";
+import { BlogPost } from "./BlogPost";
+
+type BlogPostPartial = Omit<BlogPost, "id" | "createdAt">;
+
+export async function createBLogPostHandler(event: APIGatewayEvent) {
+  const partialBlogPost: BlogPostPartial = JSON.parse(event.body!);
   return {
     statusCode: 201,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify(partialBlogPost),
   };
 }
