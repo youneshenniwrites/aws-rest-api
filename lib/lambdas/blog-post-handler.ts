67b9400d957d810cf2a06985cae7209911a6208d
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-21 18:25:15 +0100

    add additional fields to api response

--- lib/lambdas/blog-post-handler.ts
+++ lib/lambdas/blog-post-handler.ts
@@ -1,12 +1,19 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { BlogPost } from "./BlogPost";
+import { v4 as uuid } from "uuid";
 
 type BlogPostPartial = Omit<BlogPost, "id" | "createdAt">;
 
 export async function createBLogPostHandler(event: APIGatewayEvent) {
   const partialBlogPost: BlogPostPartial = JSON.parse(event.body!);
+
+  // Add missing fields
+  const id = uuid();
+  const createdAt = new Date().toISOString();
+  const blogPost: BlogPost = { id, ...partialBlogPost, createdAt };
+
   return {
     statusCode: 201,
-    body: JSON.stringify(partialBlogPost),
+    body: JSON.stringify(blogPost),
   };
 }
