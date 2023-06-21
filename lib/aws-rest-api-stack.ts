383e7a5dcc040b701500e3b0308fafb3ada93529
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-21 17:50:30 +0100

    make lambda function async

--- lib/aws-rest-api-stack.ts
+++ lib/aws-rest-api-stack.ts
@@ -1,14 +1,21 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  LambdaIntegration as ApiLambdaIntegration,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction as AWSLambdaFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { NodejsFunction as LambdaFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { join } from "path";
 
 export class AwsRestApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const api = new RestApi(this, "blogPostApi");
+
     const createBLogPostLambdaName = "createBLogPostHandler";
-    const createBLogPostLambda = new AWSLambdaFunction(
+    const createBLogPostLambda = new LambdaFunction(
       this,
       createBLogPostLambdaName,
       {
@@ -17,6 +24,13 @@ export class AwsRestApiStack extends Stack {
         handler: createBLogPostLambdaName,
         functionName: createBLogPostLambdaName,
       }
+    );
+
+    // API resources and methods
+    const blogPostPath = api.root.addResource("blogposts");
+    blogPostPath.addMethod(
+      "POST",
+      new ApiLambdaIntegration(createBLogPostLambda)
     );
   }
 }
--- lib/lambdas/blog-post-handler.ts
+++ lib/lambdas/blog-post-handler.ts
@@ -1,4 +1,4 @@
-export function createBLogPostHandler() {
+export async function createBLogPostHandler() {
   return {
     statusCode: 201,
     body: JSON.stringify({ message: "Hello World" }),
