58838e80c5fb444a7aaaca6b3c3743e945e3ef07
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-21 13:06:46 +0100

    create new cdk app

--- bin/aws-rest-api.ts
+++ bin/aws-rest-api.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { AwsRestApiStack } from '../lib/aws-rest-api-stack';
+import * as cdk from "aws-cdk-lib";
+import { AwsRestApiStack } from "../lib/aws-rest-api-stack";
 
 const app = new cdk.App();
-new AwsRestApiStack(app, 'AwsRestApiStack');
+const region = "eu-west-2";
+const account = "367985501708";
+new AwsRestApiStack(app, "AwsRestApiStack", { env: { region, account } });
