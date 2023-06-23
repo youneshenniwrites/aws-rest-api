51fefa002bf7805d5e3f59ca56964b0d54dad693
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-23 15:44:08 +0100

    add dummy data

--- bin/aws-rest-api.ts
+++ bin/aws-rest-api.ts
@@ -3,8 +3,8 @@ import * as cdk from "aws-cdk-lib";
 import { AwsRestApiStack } from "../lib/aws-rest-api-stack";
 
 const app = new cdk.App();
-const region = "eu-west-2";
-const account = "367985501708";
+const region = "us-west-1";
+const account = "123123123123";
 new AwsRestApiStack(app, "AwsRestApiStack", {
   env: { region: region, account: account },
 });
