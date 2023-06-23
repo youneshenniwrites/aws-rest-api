e04ff0b8d79e775405fd54a659b573b031dbe53d
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-23 10:21:35 +0100

    create dynamodb table

--- lib/aws-rest-api-stack.ts
+++ lib/aws-rest-api-stack.ts
@@ -3,6 +3,10 @@ import {
   LambdaIntegration as ApiLambdaIntegration,
   RestApi,
 } from "aws-cdk-lib/aws-apigateway";
+import {
+  AttributeType,
+  Table as DynamoDbTable,
+} from "aws-cdk-lib/aws-dynamodb";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction as LambdaFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
@@ -12,8 +16,17 @@ export class AwsRestApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    //* API Gateway
     const api = new RestApi(this, "blogPostApi");
 
+    //* Database
+    const dynamoDbTable = new DynamoDbTable(this, "blogPostTable", {
+      tableName: "blogPostTable",
+      //* This field is important as it serves as primary key
+      partitionKey: { name: "id", type: AttributeType.STRING },
+    });
+
+    //* Lambda Function
     const createBLogPostLambdaName = "createBLogPostHandler";
     const createBLogPostLambda = new LambdaFunction(
       this,
@@ -23,14 +36,18 @@ export class AwsRestApiStack extends Stack {
         entry: join(__dirname, "lambdas", "blog-post-handler.ts"),
         handler: createBLogPostLambdaName,
         functionName: createBLogPostLambdaName,
+        environment: { TABLE_NAME: dynamoDbTable.tableName },
       }
     );
 
-    // API resources and methods
+    //* Connect lambda to API and add resources/methods
     const blogPostPath = api.root.addResource("blogposts");
     blogPostPath.addMethod(
       "POST",
       new ApiLambdaIntegration(createBLogPostLambda)
     );
+
+    //* Grant permission for lambda to write to dynamodb
+    dynamoDbTable.grantWriteData(createBLogPostLambda);
   }
 }
--- lib/lambdas/blog-post-handler.ts
+++ lib/lambdas/blog-post-handler.ts
@@ -4,10 +4,12 @@ import { v4 as uuid } from "uuid";
 
 type BlogPostPartial = Omit<BlogPost, "id" | "createdAt">;
 
+const tableName = process.env.TABLE_NAME!;
+
 export async function createBLogPostHandler(event: APIGatewayEvent) {
   const partialBlogPost: BlogPostPartial = JSON.parse(event.body!);
 
-  // Add missing fields
+  //* Add missing fields
   const id = uuid();
   const createdAt = new Date().toISOString();
   const blogPost: BlogPost = { id, ...partialBlogPost, createdAt };
